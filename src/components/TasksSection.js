8166b71ea8f674bbd459f6012fa20a929e4f1e9d
Kirill Nazarov <urij7762@gmail.com>
2024-03-05 12:03:11 +0200

    Implemented filtering of tasks

--- src/components/TasksSection.js
+++ src/components/TasksSection.js
@@ -4,19 +4,37 @@ import { useSelector, useDispatch } from 'react-redux'
 import TaskElement from './TaskElement'
 import { toggleCompleteTask } from '../store/slices/task-slice'
 
+import { filters } from '../constants'
+
 const TodoTasks = () => {
   const allTasks = useSelector((state) => state.tasks)
   const currentFilter = useSelector((state) => state.filter)
-  console.log(currentFilter)
   const dispatch = useDispatch()
 
   const handleElementClick = (id) => {
     dispatch(toggleCompleteTask(id))
   }
 
+  const getFilteredTasks = () => {
+    switch (currentFilter) {
+      case filters.ALL: {
+        return allTasks
+      }
+      case filters.COMPLETED: {
+        return allTasks.filter((task) => task.isCompleted)
+      }
+      case filters.NOT_COMPLETED: {
+        return allTasks.filter((task) => !task.isCompleted)
+      }
+      default: {
+        return allTasks
+      }
+    }
+  }
+
   return (
     <div>
-      {allTasks.length > 0 && allTasks.map((task) => (
+      {getFilteredTasks().length > 0 && getFilteredTasks().map((task) => (
         <TaskElement
           task={task}
           key={task.id}
